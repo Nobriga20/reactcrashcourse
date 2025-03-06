5cfba63f3b666214059a43d706d2a1b842f44d66
Nobriga20 <courtvizn@gmail.com>
2025-03-05 16:10:58 -0800

    first push

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+
+import './App.css';
+import Todo from `./components/Todo.jsx`
+`
+
+function App() {
+  return (
+    <div>
+      <h1>My Todo List</h1>
+      <div>
+        <h2>Finish Frontend Simplified</h2>
+        <button>delete</button>
+      </div>
+      <div>
+        <h2>Finish Interview Section</h2>
+        <button>delete</button>
+      </div>
+      <div>
+        <h2>Land a 100k job</h2>
+        <button>delete</button>
+      </div>
+      
+  
+
+       
+
+    
+
+    
+  );
+}
+
+export default App;
